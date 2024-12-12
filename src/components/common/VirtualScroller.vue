44ef2257e8aa2373473535477905d9c64417a440
Zephyruso <176294927+Zephyruso@users.noreply.github.com>
2024-12-12 22:58:34 +0800

    refactor: virtual scroller

---
+++ src/components/common/VirtualScroller.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    ref="parentRef"
+    class="h-full w-full overflow-auto"
+  >
+    <div
+      :style="{
+        height: `${totalSize}px`,
+      }"
+      class="relative w-full"
+    >
+      <div
+        class="absolute left-0 top-0 w-full"
+        :style="{
+          transform: `translateY(${virtualRows[0]?.start ?? 0}px)`,
+        }"
+      >
+        <div
+          v-for="row in virtualRows"
+          :key="row.key.toString()"
+          :data-index="row.index"
+          :ref="measureElement"
+        >
+          <slot
+            :item="data[row.index]"
+            :index="row.index"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useVirtualizer } from '@tanstack/vue-virtual'
+import { computed, ref } from 'vue'
+
+const parentRef = ref<HTMLElement | null>(null)
+const props = defineProps<{
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: any[]
+}>()
+
+const virutalOptions = computed(() => {
+  return {
+    count: props.data.length,
+    getScrollElement: () => parentRef.value,
+    estimateSize: () => 55,
+    overscan: 48,
+  }
+})
+
+const rowVirtualizer = useVirtualizer(virutalOptions)
+
+const virtualRows = computed(() => rowVirtualizer.value.getVirtualItems())
+const totalSize = computed(() => rowVirtualizer.value.getTotalSize())
+
+const measureElement = (el: Element) => {
+  if (!el) {
+    return
+  }
+
+  rowVirtualizer.value.measureElement(el)
+
+  return undefined
+}
+</script>
--- src/views/LogsPage.vue
+++ src/views/LogsPage.vue
@@ -1,62 +1,35 @@
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col p-2">
     <template v-if="!renderLogs.length">
       <div class="card m-2 flex-row p-2 text-sm">
         {{ $t('noContent') }}
       </div>
     </template>
-    <div
-      ref="parentRef"
-      class="h-full w-full overflow-auto"
-    >
-      <div
-        :style="{
-          height: `${totalSize}px`,
-        }"
-        class="relative w-full"
-      >
-        <div
-          class="absolute left-0 top-0 w-full"
-          :style="{
-            transform: `translateY(${virtualRows[0]?.start ?? 0}px)`,
-          }"
-        >
-          <div
-            v-for="virtualLog in virtualRows"
-            :key="virtualLog.key.toString()"
-            :data-index="virtualLog.index"
-            :ref="measureElement"
-          >
-            <div class="card mx-2 mb-1 block p-2 text-sm">
-              <span>{{ renderLogs[virtualLog.index].seq }}</span>
-              <span class="mx-2 text-primary">
-                {{ dayjs(renderLogs[virtualLog.index].time).locale(language).format('HH:mm:ss') }}
-              </span>
-              <span
-                :class="
-                  textColorMapForType[
-                    renderLogs[virtualLog.index].type as keyof typeof textColorMapForType
-                  ]
-                "
-              >
-                {{ renderLogs[virtualLog.index].type }}
-              </span>
-              <span class="ml-2">{{ renderLogs[virtualLog.index].payload }}</span>
-            </div>
-          </div>
+    <VirtualScroller :data="renderLogs">
+      <template v-slot="{ item }: { item: LogWithSeq }">
+        <div class="card mb-1 block p-2 text-sm">
+          <span>{{ item.seq }}</span>
+          <span class="mx-2 text-primary">
+            {{ dayjs(item.time).locale(language).format('HH:mm:ss') }}
+          </span>
+          <span :class="textColorMapForType[item.type as keyof typeof textColorMapForType]">
+            {{ item.type }}
+          </span>
+          <span class="ml-2">{{ item.payload }}</span>
         </div>
-      </div>
-    </div>
+      </template>
+    </VirtualScroller>
   </div>
 </template>
 
 <script setup lang="ts">
+import VirtualScroller from '@/components/common/VirtualScroller.vue'
 import { LOG_LEVEL } from '@/config'
 import { logFilter, logs } from '@/store/logs'
 import { language } from '@/store/settings'
-import { useVirtualizer } from '@tanstack/vue-virtual'
+import type { LogWithSeq } from '@/types'
 import dayjs from 'dayjs'
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 
 const textColorMapForType = {
   [LOG_LEVEL.Error]: 'text-error',
@@ -74,30 +47,4 @@ const renderLogs = computed(() => {
     return true
   })
 })
-
-const parentRef = ref<HTMLElement | null>(null)
-
-const virutalOptions = computed(() => {
-  return {
-    count: renderLogs.value.length,
-    getScrollElement: () => parentRef.value,
-    estimateSize: () => 55,
-    overscan: 48,
-  }
-})
-
-const rowVirtualizer = useVirtualizer(virutalOptions)
-
-const virtualRows = computed(() => rowVirtualizer.value.getVirtualItems())
-const totalSize = computed(() => rowVirtualizer.value.getTotalSize())
-
-const measureElement = (el: Element) => {
-  if (!el) {
-    return
-  }
-
-  rowVirtualizer.value.measureElement(el)
-
-  return undefined
-}
 </script>
