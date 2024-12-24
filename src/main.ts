ba0fdc7ba8b72cef28502cc072422f12ffc1d3a4
Zephyruso <176294927+Zephyruso@users.noreply.github.com>
2024-12-24 13:55:20 +0800

    feat: dual mode for cdn fonts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,30 @@ import App from './App.vue'
 import './assets/main.css'
 import { i18n } from './i18n'
 import router from './router'
+
+if (import.meta.env.MODE === 'cdn-fonts') {
+  const createLink = (href: string) => {
+    const link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.href = href
+    link.media = 'print'
+    link.onload = () => {
+      link.media = 'all'
+    }
+    document.head.appendChild(link)
+  }
+
+  createLink('https://unpkg.com/subsetted-fonts@latest/MiSans-VF/MiSans-VF.css')
+  createLink('https://unpkg.com/subsetted-fonts@latest/SarasaUiSC-Regular/SarasaUiSC-Regular.css')
+  createLink('https://unpkg.com/subsetted-fonts@latest/PingFangSC-Regular/PingFangSC-Regular.css')
+  createLink('https://unpkg.com/@fontsource/fira-sans')
+} else {
+  import('@fontsource/fira-sans/index.css')
+  import('subsetted-fonts/MiSans-VF/MiSans-VF.css')
+  import('subsetted-fonts/SarasaUiSC-Regular/SarasaUiSC-Regular.css')
+  import('subsetted-fonts/PingFangSC-Regular/PingFangSC-Regular.css')
+}
+
 const app = createApp(App)
 
 dayjs.extend(relativeTime)
