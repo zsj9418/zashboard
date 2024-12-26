50dcc16c3059c3e5c9a1ea39ecdd61d4941e3bc1
Zephyruso <176294927+Zephyruso@users.noreply.github.com>
2024-12-26 11:53:12 +0800

    fix: rule provider style

--- src/components/rules/RuleProvider.vue
+++ src/components/rules/RuleProvider.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="card w-full flex-row items-center gap-2 p-1 px-2 text-sm">
     <div class="flex flex-1 flex-col sm:flex-row">
-      <span class="mr-2 inline-block min-w-4 text-center">{{ index }}.</span>
-      <span class="w-48 flex-1"> {{ ruleProvider.name }} ({{ ruleProvider.ruleCount }}) </span>
+      <div class="flex flex-1 items-center gap-2">
+        <span>{{ index }}.</span>
+        <span> {{ ruleProvider.name }} ({{ ruleProvider.ruleCount }}) </span>
+      </div>
       <div class="flex gap-3 text-slate-500">
         <span>{{ ruleProvider.behavior }}</span>
         <span>{{ ruleProvider.vehicleType }}</span>
