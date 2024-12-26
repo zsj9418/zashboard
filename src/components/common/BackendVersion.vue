73c56f9b49fa7d85dc2377ef919f31489ac0f35f
Zephyruso <176294927+Zephyruso@users.noreply.github.com>
2024-12-26 15:18:45 +0800

    fix: metacubex logo

--- src/components/common/BackendVersion.vue
+++ src/components/common/BackendVersion.vue
@@ -11,6 +11,6 @@
 
 <script setup lang="ts">
 import { isSingBox, version } from '@/api'
-import MetacubexLogo from '@/assets/metacubex.jpeg'
+import MetacubexLogo from '@/assets/metacubex.jpg'
 import SingBoxLogo from '@/assets/sing-box.svg'
 </script>
