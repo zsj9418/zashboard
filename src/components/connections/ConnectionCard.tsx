a11823ec50926e25e9d602bc4dc3afd0de93632c
Zephyruso <176294927+Zephyruso@users.noreply.github.com>
2024-12-30 14:54:26 +0800

    fix: connections card style

--- src/components/connections/ConnectionCard.tsx
+++ src/components/connections/ConnectionCard.tsx
@@ -26,48 +26,61 @@ export default defineComponent<{
     return () => {
       const componentMap: Record<CONNECTIONS_TABLE_ACCESSOR_KEY, JSX.Element> = {
         [CONNECTIONS_TABLE_ACCESSOR_KEY.Host]: (
-          <span class="w-80 grow break-all text-primary/80">
+          <span class="grow break-all text-primary/80">
             {props.conn.metadata.host || props.conn.metadata.destinationIP}
           </span>
         ),
         [CONNECTIONS_TABLE_ACCESSOR_KEY.Destination]: (
-          <span class="w-80 grow break-all">{props.conn.metadata.destinationIP}</span>
+          <span class="grow break-all">{props.conn.metadata.destinationIP}</span>
         ),
         [CONNECTIONS_TABLE_ACCESSOR_KEY.SourceIP]: (
-          <span class="w-80 grow break-all">{getIPLabelFromMap(props.conn.metadata.sourceIP)}</span>
+          <span class="grow break-all">{getIPLabelFromMap(props.conn.metadata.sourceIP)}</span>
         ),
         [CONNECTIONS_TABLE_ACCESSOR_KEY.SourcePort]: (
-          <span class="w-80 grow break-all">{props.conn.metadata.sourcePort}</span>
+          <span class="grow break-all">{props.conn.metadata.sourcePort}</span>
         ),
         [CONNECTIONS_TABLE_ACCESSOR_KEY.Type]: (
-          <span class="w-80 grow break-all">{props.conn.metadata.type}</span>
+          <span class="grow break-all">{props.conn.metadata.type}</span>
+        ),
+        [CONNECTIONS_TABLE_ACCESSOR_KEY.Rule]: (
+          <span class="grow break-all">
+            {props.conn.rule} {props.conn.rulePayload}
+          </span>
+        ),
+        [CONNECTIONS_TABLE_ACCESSOR_KEY.Process]: (
+          <span class="grow break-all">{getProcessFromConnection(props.conn)}</span>
+        ),
+        [CONNECTIONS_TABLE_ACCESSOR_KEY.Chains]: (
+          <span class="grow break-all">
+            {last(props.conn.chains)}=&gt;{first(props.conn.chains)}
+          </span>
         ),
         [CONNECTIONS_TABLE_ACCESSOR_KEY.Download]: (
-          <div class="w-20 whitespace-nowrap text-right">
+          <div class="whitespace-nowrap text-right">
             {prettyBytesHelper(props.conn.download)}
             <ArrowDownIcon class="-mt-1 inline-block h-4 w-4" />
           </div>
         ),
         [CONNECTIONS_TABLE_ACCESSOR_KEY.Upload]: (
-          <div class="w-20 whitespace-nowrap text-right">
+          <div class="whitespace-nowrap text-right">
             {prettyBytesHelper(props.conn.upload)}
             <ArrowUpIcon class="-mt-1 inline-block h-4 w-4" />
           </div>
         ),
         [CONNECTIONS_TABLE_ACCESSOR_KEY.DlSpeed]: (
-          <div class="w-20 whitespace-nowrap text-right">
+          <div class="whitespace-nowrap text-right">
             {prettyBytesHelper(props.conn.downloadSpeed)}/s
             <ArrowDownIcon class="-mt-1 inline-block h-4 w-4" />
           </div>
         ),
         [CONNECTIONS_TABLE_ACCESSOR_KEY.UlSpeed]: (
-          <div class="w-20 whitespace-nowrap text-right">
+          <div class="whitespace-nowrap text-right">
             {prettyBytesHelper(props.conn.uploadSpeed)}/s
             <ArrowUpIcon class="-mt-1 inline-block h-4 w-4" />
           </div>
         ),
         [CONNECTIONS_TABLE_ACCESSOR_KEY.ConnectTime]: (
-          <div class="w-20 whitespace-nowrap text-right">{fromNow(props.conn.start)}</div>
+          <div class="whitespace-nowrap text-right">{fromNow(props.conn.start)}</div>
         ),
         [CONNECTIONS_TABLE_ACCESSOR_KEY.Details]: (
           <button
@@ -84,19 +97,6 @@ export default defineComponent<{
           >
             <XMarkIcon class="h-4 w-4" />
           </button>
-        ),
-        [CONNECTIONS_TABLE_ACCESSOR_KEY.Rule]: (
-          <span class="w-80 grow break-all">
-            {props.conn.rule} {props.conn.rulePayload}
-          </span>
-        ),
-        [CONNECTIONS_TABLE_ACCESSOR_KEY.Process]: (
-          <span class="w-80 grow break-all">{getProcessFromConnection(props.conn)}</span>
-        ),
-        [CONNECTIONS_TABLE_ACCESSOR_KEY.Chains]: (
-          <span class="w-80 grow break-all">
-            {last(props.conn.chains)}=&gt;{first(props.conn.chains)}
-          </span>
         ),
       }
       return (
