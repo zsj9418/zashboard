02bf8df1807380887fba10bd06ed12932f2fd84d
Zephyruso <176294927+Zephyruso@users.noreply.github.com>
2025-02-08 15:26:31 +0800

    refactor: calc of grids cols

--- src/components/proxies/ProxyNodeGrid.vue
+++ src/components/proxies/ProxyNodeGrid.vue
@@ -1,38 +1,15 @@
 <template>
   <div
-    :class="
-      twMerge(
-        'grid grid-cols-2 gap-2 overflow-y-auto overflow-x-hidden',
-        truncateProxyName && 'max-h-96',
-        proxyCardSize === PROXY_CARD_SIZE.LARGE
-          ? isSidebarCollapsed
-            ? hasTwoColumns
-              ? 'sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5'
-              : 'sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 3xl:grid-cols-10'
-            : hasTwoColumns
-              ? 'sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-4'
-              : 'sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 3xl:grid-cols-8'
-          : isSidebarCollapsed
-            ? hasTwoColumns
-              ? 'sm:grid-cols-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6'
-              : 'sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-10 3xl:grid-cols-12'
-            : hasTwoColumns
-              ? 'sm:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5'
-              : 'sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8 3xl:grid-cols-10',
-      )
-    "
+    class="grid max-h-96 gap-2 overflow-y-auto overflow-x-hidden"
+    :style="`grid-template-columns: repeat(auto-fill, minmax(${proxyCardSize === PROXY_CARD_SIZE.LARGE ? 150 : 138}px, 1fr));`"
   >
     <slot />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { useProxies } from '@/composables/proxies'
 import { PROXY_CARD_SIZE } from '@/config'
-import { isSidebarCollapsed, proxyCardSize, truncateProxyName } from '@/store/settings'
-import { twMerge } from 'tailwind-merge'
-
-const { hasTwoColumns } = useProxies()
+import { proxyCardSize } from '@/store/settings'
 </script>
 
 <style scoped>
--- src/composables/proxies.ts
+++ src/composables/proxies.ts
@@ -2,7 +2,7 @@ import { isSingBox } from '@/api'
 import { PROXY_TAB_TYPE } from '@/config'
 import { configs } from '@/store/config'
 import { GLOBAL, hiddenGroupMap, proxyGroupList, proxyProviederList } from '@/store/proxies'
-import { manageHiddenGroup, twoColumnProxyGroup } from '@/store/settings'
+import { manageHiddenGroup } from '@/store/settings'
 import { computed, ref } from 'vue'
 
 export const proxiesFilter = ref('')
@@ -27,12 +27,10 @@ const renderGroups = computed(() => {
     ? proxyGroups
     : proxyGroups.filter((name) => !hiddenGroupMap.value[name])
 })
-const hasTwoColumns = computed(() => twoColumnProxyGroup.value && renderGroups.value.length > 1)
 
 export const useProxies = () => {
   return {
     proxiesTabShow,
     renderGroups,
-    hasTwoColumns,
   }
 }
